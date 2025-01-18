42527ce4978cf482b21bebbfce4522bd0f06ba44
erkankaradag <erkankaradagcom@gmail.com>
2025-01-19 00:49:32 +0300

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace TryCath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Lütfen bir sayı giriniz:");
+                    int sayi = Convert.ToInt32(Console.ReadLine());
+                    int sayininKaresi = sayi * sayi;
+                    Console.WriteLine(sayininKaresi);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Lütfen geçerli bir sayı girin!");
+                }
+            }
+        }
+    }
+}
